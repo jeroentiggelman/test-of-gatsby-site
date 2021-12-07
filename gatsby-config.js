f8012db9d9ad45d526dba842057e16397354d5c3
jeroentiggelman <infos@photographicdesign.be>
2021-12-07 12:27:15 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Test of Gatsby site",
-  },
-  plugins: [],
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Test of Gatsby site",
+    },
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+                alt="Male warrior, looking stoically at the camera"
+                src="../images/20210807-warriors_2JT2327-1.jpg"
+            />
         </Layout>
     );
 };
